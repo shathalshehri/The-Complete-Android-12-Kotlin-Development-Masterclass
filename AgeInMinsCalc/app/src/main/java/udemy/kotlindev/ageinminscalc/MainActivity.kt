78172d8d5e80fbe999bad1e82ac2024164d0a128
Shatha Salem <shathasalem.cs@gmail.com>
2023-06-23 23:14:50 +0300

    Second App

---
+++ AgeInMinsCalc/app/src/main/java/udemy/kotlindev/ageinminscalc/MainActivity.kt
@@ -0,0 +1,11 @@
+package udemy.kotlindev.ageinminscalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
